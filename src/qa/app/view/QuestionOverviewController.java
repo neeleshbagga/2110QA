104df59cf07049e1ebea73e0d69dbf1c7869babc
neeleshbagga <nb287@cornell.edu>
2015-01-23 08:07:00 -0500

    Error checks
    - can't view explanation without submit
    - can’t submit without choosing

--- src/qa/app/view/QuestionOverviewController.java
+++ src/qa/app/view/QuestionOverviewController.java
@@ -34,37 +34,37 @@ public class QuestionOverviewController {
 	private Button next;
 	@FXML
 	private Button submit;
+	boolean submitPressed = false;
+
 	@FXML
 	private Button explain;
-	
+
 	@FXML
 	private TextField userSolCorrect;
-	
+
 	@FXML
 	private TextArea explainArea;
-	
+
 	@FXML
 	private ChoiceBox<String> choiceBox;
-	
+
 	@FXML
 	private ProgressBar progressBar;
-	 
+
 	// Questions corresponding to currently selected topic
 	private Iterator<Question> currentQuestions;
-	
-	
+
 	private ArrayList<Question> allQuestions;
-	
+
 	private ObservableList<String> answerChoices;
 	// Reference to the main application.
 	private MainApp mainApp;
-	
+
 	// the current question that the user sees.
 	private Question currQ;
-	
+
 	private int count = 0;
-	
-	
+
 	private HashMap<String, Integer> topicLength = new HashMap<String, Integer>();
 
 	/**
@@ -72,102 +72,117 @@ public class QuestionOverviewController {
 	 * method.
 	 */
 	public QuestionOverviewController() {
-		
+
 	}
-	
-	
+
 	/**
 	 * Next button pressed
 	 */
 	@FXML
-	public void nextPressed(){
+	public void nextPressed() {
 		displayNextQuestion();
 	}
-	
-	
 
 	/**
 	 * Submit button pressed
 	 */
 	@FXML
-	public void submitPressed(){
+	public void submitPressed() {
 		String selection = choiceBox.getSelectionModel().getSelectedItem();
-		
-		if(selection.equals(currQ.getAnswer().getValue()))
-			userSolCorrect.setText("Your answer was CORRECT");
-		else
-			userSolCorrect.setText("Your answer was INCORRECT");
-		
-		userSolCorrect.setVisible(true);
+		if (selection == null) {
+			Dialogs.create()
+			.title("Error:")
+			.masthead("Make a selection, and then submit.")
+			.message(
+					"")
+			.showWarning();
+
+		} else {
+			submitPressed = true;
+			if (selection.equals(currQ.getAnswer().getValue()))
+				userSolCorrect.setText("Your answer was CORRECT");
+			else
+				userSolCorrect.setText("Your answer was INCORRECT");
+
+			userSolCorrect.setVisible(true);
+		}
 	}
-	
+
 	/**
 	 * Explain button pressed
 	 */
 	@FXML
-	public void explainPressed(){
-		explainArea.setText(currQ.getAnswer().getExplain());
-		explainArea.setVisible(true);
+	public void explainPressed() {
+		if (submitPressed) {
+			explainArea.setText(currQ.getAnswer().getExplain());
+			explainArea.setVisible(true);
+		} else {
+			Dialogs.create()
+					.title("")
+					.masthead("Question needs to be attempted.")
+					.message(
+							"Sorry, please answer the question and then try to view the explanation.")
+					.showWarning();
+		}
 	}
-	
-	
+
 	/**
 	 * Switch the display, transition to next question if one exists.
 	 */
-	private void displayNextQuestion(){
-		
-		
+	private void displayNextQuestion() {
+
 		userSolCorrect.setVisible(false);
 		explainArea.setVisible(false);
-		count ++;
-		progressBar.setProgress((double)count/topicLength.get(currQ.getTopic().getTopicName()));
-		
-		if(currentQuestions.hasNext()){
+		submitPressed = false;
+		count++;
+		progressBar.setProgress((double) count
+				/ topicLength.get(currQ.getTopic().getTopicName()));
+
+		if (currentQuestions.hasNext()) {
 			currQ = currentQuestions.next();
 			textArea.setText(currQ.getText());
 			updateAnswerChoices(currQ);
-			
-			
-		}
-		else
-		{
-			
+
+		} else {
+
 			Dialogs.create()
-            .title("Topic Completed")
-            .masthead("No more questions")
-            .message("Kudos. You have completed all the questions in this topic. Please select another topic to proceed")
-            .showWarning();
+					.title("Topic Completed")
+					.masthead("No more questions")
+					.message(
+							"Kudos. You have completed all the questions in this topic. Please select another topic to proceed")
+					.showWarning();
 		}
 	}
-	
+
 	/**
-	 * Fills all text fields to show details about the person.
-	 * If the specified person is null, all text fields are cleared.
+	 * Fills all text fields to show details about the person. If the specified
+	 * person is null, all text fields are cleared.
 	 * 
-	 * @param person the person or null
+	 * @param person
+	 *            the person or null
 	 */
 	private void showTopicQuestions(Topic topic) {
 		count = 0;
 		progressBar.setProgress(0);
 		userSolCorrect.setVisible(false);
 		explainArea.setVisible(false);
-		
-		currentQuestions = QAUtils.filterQuestionsByTopic(allQuestions, topic).iterator();
-	    
-	   
-	    if(currentQuestions.hasNext()){
-	    	currQ = currentQuestions.next();
-	    	textArea.setText(currQ.getText());
-	    	updateAnswerChoices(currQ);
-	    }
+		submitPressed = false;
+
+		currentQuestions = QAUtils.filterQuestionsByTopic(allQuestions, topic)
+				.iterator();
+
+		if (currentQuestions.hasNext()) {
+			currQ = currentQuestions.next();
+			textArea.setText(currQ.getText());
+			updateAnswerChoices(currQ);
+		}
 	}
-	
-	private void updateAnswerChoices(Question currQ){
+
+	private void updateAnswerChoices(Question currQ) {
 		answerChoices = currQ.getanswerChoices();
-    	choiceBox.setItems(answerChoices);
+		choiceBox.setItems(answerChoices);
 	}
-	
-	
+
 	/**
 	 * Initializes the controller class. This method is automatically called
 	 * after the fxml file has been loaded.
@@ -177,17 +192,20 @@ public class QuestionOverviewController {
 		// Initialize the person table with the two columns.
 		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue()
 				.firstNameProperty());
-		
-	    //  showTopicQuestions(null);
 
-	    // Listen for selection changes and show the person details when changed.
-	    topicTable.getSelectionModel().selectedItemProperty().addListener(
-	            (observable, oldValue, newValue) -> showTopicQuestions(newValue));
-	    
-	    progressBar.setProgress(0);
+		// showTopicQuestions(null);
+
+		// Listen for selection changes and show the person details when
+		// changed.
+		topicTable
+				.getSelectionModel()
+				.selectedItemProperty()
+				.addListener(
+						(observable, oldValue, newValue) -> showTopicQuestions(newValue));
+
+		progressBar.setProgress(0);
 	}
 
-	
 	/**
 	 * Is called by the main application to give a reference back to itself.
 	 * 
@@ -195,14 +213,12 @@ public class QuestionOverviewController {
 	 */
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
-	
+
 		// Add observable list data to the table
 		topicTable.setItems(mainApp.getTopics());
 		allQuestions = mainApp.getQuestions();
 		topicLength = QAUtils.initTopicLengthMap(allQuestions);
-		
+
 	}
-	
-	
-	
+
 }
